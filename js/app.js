7237dd525bf0d6dfbb62da56a4d3166b73c775c5
omor777 <omor0843@gmail.com>
2024-02-27 16:03:43 +0600

    show rgb color on placeholder

--- js/app.js
+++ js/app.js
@@ -11,32 +11,38 @@ function main() {
   const allInputs = document.getElementsByClassName(`input-range
   input-range
   input-range`);
+  const rgb = id("rbg");
+
+  function showRgbColor() {
+    const rgbaColor = display.style.backgroundColor;
+    rgb.placeholder = rgbaColor;
+  }
 
   redInput.addEventListener("change", function () {
     const red = this.value;
     const green = getInputValue("green");
     const blue = getInputValue("blue");
-
     const rgbColor = `rgb(${red}, ${green}, ${blue})`;
     display.style.backgroundColor = rgbColor;
+    showRgbColor();
   });
 
   greenInput.addEventListener("change", function () {
     const red = getInputValue("red");
     const green = this.value;
     const blue = getInputValue("blue");
-
     const rgbColor = `rgb(${red}, ${green}, ${blue})`;
     display.style.backgroundColor = rgbColor;
+    showRgbColor();
   });
 
   blueInput.addEventListener("click", function () {
     const red = getInputValue("red");
     const green = getInputValue("green");
     const blue = this.value;
-
     const rgbColor = `rgb(${red}, ${green},${blue})`;
     display.style.backgroundColor = rgbColor;
+    showRgbColor();
   });
 }
 
